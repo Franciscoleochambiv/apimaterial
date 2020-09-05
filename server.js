e8eafda2481dbed116d49219aba7d7de308590a2
frank <grupo90pr@gmail.com>
2020-09-05 18:57:34 -0500

    servidor local1

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.use(myConnection(mysql, {
     host: '201.230.180.172',
     user: 'fran',
     password: '12345678',
-    port: 12180,
+    port: 3360,
     database: 'serapio'
 }, 'single'));
 
