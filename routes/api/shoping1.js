f23865d44189123424a2795b742dfca76f41596d
frank <grupo90pr@gmail.com>
2020-09-12 12:32:20 -0500

    make it version87

--- routes/api/shoping1.js
+++ routes/api/shoping1.js
@@ -434,27 +434,26 @@ router.post("/card", async (req, res) => {
       //var cadena= "Insert into DocVentaCabweb(DVC_Serie,DVC_) value ('1'"+"" +")";
      // var  cadena3="0";
 
+     var numero_ticket='200';
+     var fecha ='now()';
+     var tdoc='30';
+     var ncli='1';
+     var pagado='0';
+     var fpago="'CONTADO'";
+     var observa="'onservaciones'";
+     var vendedor="'taqyu'";
+     var dvcanulado='0';
+     var guia='0';
+     var caja='1';
+     var Empresa='1';
+     var dvcnc='0';
+     var serien='0';
+     var dvcsaldo='0';
+     var pendiente='0';
 
-      var seriec ='1';
-      var numero_ticket='200';
-      var fecha ='now()';
-      var tdoc='30';
-      var ncli='1';
-      var pagado='0';
-      var fpago='CONTADO';
-      var observa='onservaciones';
-      var vendedor='taqyu';
-      var dvcanulado='0';
-      var guia='0';
-      var caja='1';
-      var Empresa='1';
-      var dvcnc='0';
-      var serien='';
-      var dvcsaldo='0';
-      var pendiente='0';
-
-     var  cadena="Insert into DocVentaCabweb(DVC_Serie,DVC_Numero,DVC_Fecha,DVC_FechaIng,TD_ID,PVCL_ID,DVC_Pagado,DVC_FormaPago,DVC_Observaciones,DVC_Vendedor,DVC_Anulado,DVC_Guia,Alm_Id,Empresa,DVC_NC,serien,DVC_Saldo,Pendiente) values(";
-     var  cadena2=cadena+seriec+","+numero_ticket+","+fecha+","+fecha+","+tdoc+","+ncli+","+pagado+","+fpago+","+observa+","+vendedor+","+dvcanulado+","+guia+","+caja+","+Empresa+","+dvcnc+","+serien+","+dvcsaldo+","+pendiente+")";
+    var  cadena="Insert into DocVentaCabweb(DVC_Serie,DVC_Numero,DVC_Fecha,DVC_FechaIng,TD_ID,PVCL_ID,DVC_Pagado,DVC_FormaPago,DVC_Observaciones,DVC_Vendedor,DVC_Anulado,DVC_Guia,Alm_Id,Empresa,DVC_NC,serien,DVC_Saldo,Pendiente) values(";
+    var  cadena2=cadena+seriec+","+numero_ticket+","+fecha+","+fecha+","+tdoc+","+ncli+","+pagado+","+fpago+","+observa+","+vendedor+","+dvcanulado+","+guia+","+caja+","+Empresa+","+dvcnc+","+serien+","+dvcsaldo+","+pendiente+")";
+     
 
 
 
@@ -464,7 +463,7 @@ router.post("/card", async (req, res) => {
                 res.json(err);
             }
     
-            res.json(customers);
+             res.json(customers);
             conn.release();
             //res.render('customers', {
              //   data: customers
